3f9c127a18148e79c459568102958a37b929d0fe
Ирина Орлова <ir.orlova@mail.astondevs.ru>
2025-02-13 15:40:33 +0000

    Revert "Merge branch 'feat_EVERGREEN-322' into 'develop'"
    This reverts merge request !7

--- src/main/java/aston/lab/clientserver/data/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package aston.lab.clientserver.data.repository;
-
-import aston.lab.clientserver.data.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-
-}
